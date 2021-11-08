058d36e928957e361cfab83560938c489160e8e7
Adenine <30560532+jisellejones@users.noreply.github.com>
2021-11-07 16:53:04 -0800

    removed python connection code from app.py file

--- app.py
+++ app.py
@@ -2,10 +2,6 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for, 
 from flask_sqlalchemy import SQLAlchemy
 
-engine = pg.connect("dbname='d3r8dfuncb78iv' user='jrufhfiejfajri' host='ec2-52-200-155-213.compute-1.amazonaws.com' port='5432' password='9a7254d2151b5e3c280fe275dbba039acdc9190fbc167f64c564c449ca77af88'")
-fire_df = pd.read_sql('select * from wildfire_data', con=engine)
-noaa_df = pd.read_sql('select * from noaa_data', con=engine)
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
