d9b9f464348eeae1283c734b48bc15f9b0ab0d58
Adenine <30560532+jisellejones@users.noreply.github.com>
2021-11-07 17:07:34 -0800

    connected app.py to index.html

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 # Initial Import of Dependencies
-from flask import Flask, render_template, jsonify, request, redirect, url_for, 
+from flask import Flask , render_template, jsonify, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config = ['SQLALCHEMY_DATABASE_URI'] = "postgres://jrufhfiejfajri:9a7254d2151b5e3c280fe275dbba039acdc9190fbc167f64c564c449ca77af88@ec2-52-200-155-213.compute-1.amazonaws.com:5432/d3r8dfuncb78iv"] 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://jrufhfiejfajri:9a7254d2151b5e3c280fe275dbba039acdc9190fbc167f64c564c449ca77af88@ec2-52-200-155-213.compute-1.amazonaws.com:5432/d3r8dfuncb78iv"
 
 db = SQLAlchemy(app)
 
 @app.route('/')
 def welcome():
-    return(
-    '''
-    Welcome to the Wildfire Prediction Page!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
-    ''')
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
