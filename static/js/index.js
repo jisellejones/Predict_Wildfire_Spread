b2485fd66e6934b5d482cf99c2214d78babab09e
Michael Freel <michaelfreel@MikesMBPro.local>
2021-11-24 19:28:27 -0800

    HTML/CSS/JS

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 // for barplot
 
-d3.json("give url to api you created from app.py").then(function(result) {
+d3.json("/api/features/fire").then(function(result) {
     console.log(result)
     x = result.map(x => x.max)
     console.log(x)
