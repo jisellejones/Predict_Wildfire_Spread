7bdce1465cf9837fbd267903788f67c56953ddbe
Adenine <30560532+jisellejones@users.noreply.github.com>
2021-11-27 11:29:59 -0800

    trying to deploy

--- static/js/wildfiremap.js
+++ static/js/wildfiremap.js
@@ -1,5 +1,5 @@
 // Add map tile
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={mapBoxToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: mapBoxToken
